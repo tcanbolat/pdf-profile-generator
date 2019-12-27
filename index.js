1fe68ebf48a71ad7f33fbd0bd8700a191db909d7
atcanbolat <kangalcanbolat@gmail.com>
2019-12-27 12:59:04 -0800

    creted axios function to get info and display on an html

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
 const inquirer = require("inquirer");
-const generateHTML = require("./generateHTML.js");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
+const generateHTML = require("./generateHTML");
+
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const promptUser = () => {
-    return inquirer.prompt([
-      {
-          type: "input",
-          name: "name",
-          message: "what is your gitHub username?"
-      }  
-    ]);
-}
-
+promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "what is your gitHub username?"
+    },
+    {
+      type: "list",
+      name: "color",
+      message: "What color would you like your profile's template to be?",
+      choices: [
+        "green",
+        "blue",
+        "pink",
+        "red"
+      ]
+    },
+  ]);
+};
 promptUser()
-.then( (answer)=> {
-console.log(answer.name)
-});
 
-const questions = [
-  
-];
+  .then( (data) => {
 
-function writeToFile(fileName, data) {
- 
-}
+    const queryUrl = `https://api.github.com/users/${data.name}`;
+    const starsUrl = `https://api.github.com/users/${data.name}/starred`;
+    axios.get(queryUrl, starsUrl).then( (res, stars) => {
+      html = generateHTML(data, res, stars);
+      writeFileAsync("index.html", html);
+    //  console.log(res.data);
+      console.log(res.data.login); 
+      console.log(res.data.location);
+      console.log(res.data.url);
+      console.log(res.data.blog);
+      console.log(res.data.bio);
+      console.log(res.data.public_repos);
+      console.log(res.data.followers);
+      console.log(res.data.following);
+     // console.log(stars.data);
+    })
+   
+    // axios.get(queryUrlStars).then(function(stars) {
+      
 
-function init() {
-}
-init();
+      
+    // });
+
+
+  }).catch( (err) => {
+    console.log(err);
+
+  });
+
+
+
+
+// function init() {
+// }
+// init();
